313c5b6ad3df2d299f99b2e74e69ca932f8a47a1
qJay44 <m70155302@yandex.ru>
2022-10-15 19:26:38 +0300

    108 / 804

--- AndroidStudio/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ AndroidStudio/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageButton
@@ -11,11 +12,16 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private companion object {
+        private const val TAG = "MainActivity"
+    }
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var prevButton: ImageButton
     private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
+    private var points = 0.0f
     private var currentIndex = 0
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -28,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -56,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
+            if (currentIndex == questionBank.size - 1) return@setOnClickListener
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
@@ -65,9 +73,15 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResIdNew = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResIdNew)
+
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
@@ -75,6 +89,14 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
 
+        if (userAnswer == correctAnswer) points++
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        if (currentIndex == questionBank.size - 1) {
+            val successRate = (points / questionBank.size * 100.0f)
+            val message = if (successRate > 100.0f) "100%" else successRate.toString().plus("%")
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
